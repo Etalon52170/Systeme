f2e8d1a27f6d08eb4f362af93e57e09b6991e0a0
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-02-11 15:50:28 +0100

    ajoute main et constantes prof

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ import java.io.InputStreamReader;
 
 public class Main  extends Constantes { // Programme de simulation d'une Ligne De Caisses
 
-private static boolean assert_flag = false; 
+    private static boolean assert_flag = false; 
 
-public static void main (String args[]) {
+    public static void main (String args[]) {
 	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	Echeancier echeancier = new Echeancier();
 	LigneDeCaisses ligneDeCaisses = new LigneDeCaisses(echeancier);
@@ -17,40 +17,43 @@ public static void main (String args[]) {
 	assert (ligneDeCaisses.debugCheckAssertOnly());
      	// Boucle principale du simulateur: 
 	while ( ! echeancier.estVide() ) {
-		if ( loop == 1 ) {
-			System.out.print("----- Etat actuel du simulateur (nombre total de pas = ");
-				System.out.print(simulationStepCounter);
-				if ( assert_flag ) {
-					System.out.print(", assert ON");
-				} else {
-					System.out.print(", assert OFF");
-				}
-				System.out.println(") -----");
-				ligneDeCaisses.affiche();
-				echeancier.affiche(ligneDeCaisses);
-			}
-			if (loop == 1) {
-				System.out.println(
-					"Taper \"Enter\" ou le nombre de pas de simulation que vous voulez réaliser");
-				try {
-					loop = Integer.parseInt(input.readLine());
-					if (loop < 0) {
-						loop = 1;
-					}
-				} catch (Exception e) {
-					loop = 1;
-				}
-			} else {
-				loop--;
-			}
-			Evenement evenement = echeancier.retourneEtEnlevePremier();
-			evenement.traiter(ligneDeCaisses, echeancier);
-			assert (ligneDeCaisses.debugCheckAssertOnly());
+	    if ( loop == 1 ) {
+		System.out.print("----- Etat actuel du simulateur (nombre total de pas = ");
+		System.out.print(simulationStepCounter);
+		if ( assert_flag ) {
+		    System.out.print(", assert ON");
+		} else {
+		    System.out.print(", assert OFF");
+		}
+		System.out.println(") -----");
+		ligneDeCaisses.affiche();
+		echeancier.affiche(ligneDeCaisses);
+	    }
+	    if (loop == 1) {
+		System.out.println(
+                "Taper \"Enter\" ou le nombre de pas de simulation que vous voulez réaliser");
+		try {
+		    loop = Integer.parseInt(input.readLine());
+		    if ( loop == 1 ) {
+			break;
+		    }
+		    else if (loop < 0) {
+			loop = 1;
+		    }
+		} catch (Exception e) {
+		    loop = 1;
+		}
+	    } else {
+		loop--;
+	    }
+	    Evenement evenement = echeancier.retourneEtEnlevePremier();
+	    evenement.traiter(ligneDeCaisses, echeancier);
+	    assert (ligneDeCaisses.debugCheckAssertOnly());
 	    ligneDeCaisses.debugCheckAssertOnly(); // Pour ceux qui vont oublier de mettre les assert... à commenter pour aller plus vite !
 	    assert echeancier.debugCheckAssertOnly();
 	    echeancier.debugCheckAssertOnly();  // Pour ceux qui vont oublier de mettre les assert... à commenter pour aller plus vite !
 	    simulationStepCounter++;
 	}
 	System.out.println("********** SIMULATION TERMINEE **********");
-}
+    }
 }
